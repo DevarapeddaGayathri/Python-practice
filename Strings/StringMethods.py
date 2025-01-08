fd775ca8b238b75f1ed1c42c840b429980f686ad
Gayathri <devarapeddagayathri@gmail.com>
2025-01-08 16:59:19 +0530

    String programs

---
+++ Strings/StringMethods.py
@@ -0,0 +1,14 @@
+#Different ways of declaring the strings:
+'''
+
+'''
+s1="My name is Gayathri"
+s2 ="My age is 20"
+s3="""We are learning
+     strings"""
+s4 = '''Python is Dynamically 
+    Typed langauge'''
+print(s1)
+print(s2)
+print(s3)
+print(s4)
