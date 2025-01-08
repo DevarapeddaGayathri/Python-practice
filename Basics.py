2ef295f75db03463c70c52463f793429071ae344
Gayathri <devarapeddagayathri@gmail.com>
2025-01-08 16:42:49 +0530

    Initial Commit

---
+++ Basics.py
@@ -0,0 +1,9 @@
+#Scripting way for writing the code:
+print('Scripting way of printing Hello World')
+
+#Procedural Way of writing the code
+def message():
+    print('Procedural way of writing: Hello world')
+message()
+
+#Object-oriented way of writing code
